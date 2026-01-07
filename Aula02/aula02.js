db7e64b3230935da5114adeba28d3d0e41ef31a9
Gustavo Dias <lima364551441@outlook.com>
2026-01-07 15:48:41 -0300

    Adicionando arquivos - Tipos de saídas

---
+++ Aula02/aula02.js
@@ -0,0 +1,5 @@
+//formas de saídas.
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!"; //Pegar o elemento na página html com id "texto".
+//Serve para mudar o conteúdo de um elemento HTML
+alert(10+5); //Janela de aviso no navegador
+console.log("Isso é um alerta!!"); //Mostra mensagens no console do navegador
